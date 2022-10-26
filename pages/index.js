96b23e07c75230c86ede62522dbf410f8346a6bd
sterphone <100229938+sterphone@users.noreply.github.com>
2022-10-26 18:09:19 -0400

    Update index.js
    added David text

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
-      <h2>Hello, my name is Diego Gallardo</h2>
+      <h2>Hello, my name is David</h2>
     </div>
   )
 }
